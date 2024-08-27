c9172e64182a4c7fd09eb3219e5418c693110e69
Ashwin <ashwins.2019a@vitstudent.ac.in>
2024-08-28 01:50:59 +0530

    iteration-1

--- helpers/run.py
+++ helpers/run.py
@@ -1,10 +1,12 @@
 import tempfile, os
 
-def run_file_generation(file):
+def run_file_generation(file, isProposal=False):
     try:
         with tempfile.TemporaryDirectory(delete=False) as temp_dir:
             temp_file_path = os.path.join(temp_dir, file.filename)
-
+            if isProposal: 
+                filename = file.filename.split('/')[-1]
+                temp_file_path = os.path.join(temp_dir, filename)
             with open(temp_file_path, 'wb') as temp_file:
                 content = file.file.read()
                 temp_file.write(content)
